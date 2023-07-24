65396992e5a10c7eef2ee36ad6dd776cb889f520
RolandM99 <manfulmweze99@gmail.com>
2023-07-24 15:54:47 +0200

    Implement the feature of finding different blog post by slug

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports.SITE = {
+  name: 'CREI',
+
+  origin: 'https://crei-rdc.vercel.app',
+  basePathname: '/',
+  trailingSlash: false,
+
+  title: 'CREI',
+  description: 'Official website of CREI',
+};
