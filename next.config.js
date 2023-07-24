2fd7ddad0f175e43548e4470aace60a93e128c99
RolandM99 <manfulmweze99@gmail.com>
2023-07-24 16:06:20 +0200

    Setup project and configure some best practices tools for clean code

---
+++ next.config.js
@@ -0,0 +1,33 @@
+const SITE = require('./src/config.js').SITE;
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+
+  trailingSlash: SITE.trailingSlash,
+  basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
+
+  swcMinify: true,
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      }
+    ],
+  },
+  experimental: { appDir: true },
+};
